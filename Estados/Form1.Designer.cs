047391ab79db567f68e2aaf72ea8aeb459847fc6
Usuario <Usuario@CPD02-M18>
2024-08-27 12:22:57 -0300

    Adicionar arquivos de projeto.

---
+++ Estados/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Estados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Se = new System.Windows.Forms.Label();
+            this.S = new System.Windows.Forms.Label();
+            this.Sc = new System.Windows.Forms.ComboBox();
+            this.SEc = new System.Windows.Forms.ComboBox();
+            this.CO = new System.Windows.Forms.ComboBox();
+            this.NE = new System.Windows.Forms.ComboBox();
+            this.N = new System.Windows.Forms.ComboBox();
+            this.TE = new System.Windows.Forms.ComboBox();
+            this.BTE = new System.Windows.Forms.Button();
+            this.BN = new System.Windows.Forms.Button();
+            this.BNE = new System.Windows.Forms.Button();
+            this.BCO = new System.Windows.Forms.Button();
+            this.BSE = new System.Windows.Forms.Button();
+            this.BS = new System.Windows.Forms.Button();
+            this.NTE = new System.Windows.Forms.Label();
+            this.NN = new System.Windows.Forms.Label();
+            this.NNE = new System.Windows.Forms.Label();
+            this.NCO = new System.Windows.Forms.Label();
+            this.NS = new System.Windows.Forms.Label();
+            this.NSE = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Todos os Estados:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Norte:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nordeste:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Centro-Oste:";
+            // 
+            // Se
+            // 
+            this.Se.AutoSize = true;
+            this.Se.Location = new System.Drawing.Point(89, 166);
+            this.Se.Name = "Se";
+            this.Se.Size = new System.Drawing.Size(51, 13);
+            this.Se.TabIndex = 4;
+            this.Se.Text = "Suldeste:";
+            // 
+            // S
+            // 
+            this.S.AutoSize = true;
+            this.S.Location = new System.Drawing.Point(115, 190);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(25, 13);
+            this.S.TabIndex = 5;
+            this.S.Text = "Sul:";
+            // 
+            // Sc
+            // 
+            this.Sc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Sc.FormattingEnabled = true;
+            this.Sc.Location = new System.Drawing.Point(148, 190);
+            this.Sc.Name = "Sc";
+            this.Sc.Size = new System.Drawing.Size(121, 21);
+            this.Sc.TabIndex = 6;
+            // 
+            // SEc
+            // 
+            this.SEc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SEc.FormattingEnabled = true;
+            this.SEc.Location = new System.Drawing.Point(148, 163);
+            this.SEc.Name = "SEc";
+            this.SEc.Size = new System.Drawing.Size(121, 21);
+            this.SEc.TabIndex = 7;
+            // 
+            // CO
+            // 
+            this.CO.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CO.FormattingEnabled = true;
+            this.CO.Location = new System.Drawing.Point(148, 136);
+            this.CO.Name = "CO";
+            this.CO.Size = new System.Drawing.Size(121, 21);
+            this.CO.TabIndex = 8;
+            // 
+            // NE
+            // 
+            this.NE.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.NE.FormattingEnabled = true;
+            this.NE.Location = new System.Drawing.Point(148, 109);
+            this.NE.Name = "NE";
+            this.NE.Size = new System.Drawing.Size(121, 21);
+            this.NE.TabIndex = 9;
+            // 
+            // N
+            // 
+            this.N.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.N.FormattingEnabled = true;
+            this.N.Location = new System.Drawing.Point(148, 82);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(121, 21);
+            this.N.TabIndex = 10;
+            // 
+            // TE
+            // 
+            this.TE.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TE.FormattingEnabled = true;
+            this.TE.Items.AddRange(new object[] {
+            "Acre - AC",
+            "Alagoas - AL",
+            "Amapá - AP",
+            "Amazonas - AM",
+            "Bahia - BA",
+            "Ceará - CE",
+            "Distrito Federal - DF",
+            "Espírito Santo - ES",
+            "Goiás - GO",
+            "Maranhão - MA",
+            "Mato Grosso - MT",
+            "Mato Grosso do Sul - MS",
+            "Minas Gerais - MG",
+            "Pará - PA",
+            "Paraíba - PB",
+            "Paraná - PR",
+            "Pernambuco - PE",
+            "Piauí - PI",
+            "Roraima - RR",
+            "Rondônia - RO",
+            "Rio de Janeiro - RJ",
+            "Rio Grande do Norte - RN",
+            "Rio Grande do Sul - RS",
+            "Santa Catarina - SC",
+            "São Paulo - SP",
+            "Sergipe - SE",
+            "Tocantins - TO"});
+            this.TE.Location = new System.Drawing.Point(148, 55);
+            this.TE.Name = "TE";
+            this.TE.Size = new System.Drawing.Size(121, 21);
+            this.TE.TabIndex = 11;
+            // 
+            // BTE
+            // 
+            this.BTE.Location = new System.Drawing.Point(283, 56);
+            this.BTE.Name = "BTE";
+            this.BTE.Size = new System.Drawing.Size(51, 19);
+            this.BTE.TabIndex = 12;
+            this.BTE.Text = "----";
+            this.BTE.UseVisualStyleBackColor = true;
+            this.BTE.Click += new System.EventHandler(this.BTE_Click);
+            // 
+            // BN
+            // 
+            this.BN.Location = new System.Drawing.Point(283, 85);
+            this.BN.Name = "BN";
+            this.BN.Size = new System.Drawing.Size(51, 19);
+            this.BN.TabIndex = 18;
+            this.BN.Text = "----";
+            this.BN.UseVisualStyleBackColor = true;
+            // 
+            // BNE
+            // 
+            this.BNE.Location = new System.Drawing.Point(283, 112);
+            this.BNE.Name = "BNE";
+            this.BNE.Size = new System.Drawing.Size(51, 19);
+            this.BNE.TabIndex = 19;
+            this.BNE.Text = "----";
+            this.BNE.UseVisualStyleBackColor = true;
+            // 
+            // BCO
+            // 
+            this.BCO.Location = new System.Drawing.Point(283, 139);
+            this.BCO.Name = "BCO";
+            this.BCO.Size = new System.Drawing.Size(51, 19);
+            this.BCO.TabIndex = 20;
+            this.BCO.Text = "----";
+            this.BCO.UseVisualStyleBackColor = true;
+            // 
+            // BSE
+            // 
+            this.BSE.Location = new System.Drawing.Point(283, 163);
+            this.BSE.Name = "BSE";
+            this.BSE.Size = new System.Drawing.Size(51, 19);
+            this.BSE.TabIndex = 21;
+            this.BSE.Text = "----";
+            this.BSE.UseVisualStyleBackColor = true;
+            // 
+            // BS
+            // 
+            this.BS.Location = new System.Drawing.Point(283, 192);
+            this.BS.Name = "BS";
+            this.BS.Size = new System.Drawing.Size(51, 19);
+            this.BS.TabIndex = 22;
+            this.BS.Text = "----";
+            this.BS.UseVisualStyleBackColor = true;
+            // 
+            // NTE
+            // 
+            this.NTE.AutoSize = true;
+            this.NTE.Location = new System.Drawing.Point(349, 62);
+            this.NTE.Name = "NTE";
+            this.NTE.Size = new System.Drawing.Size(19, 13);
+            this.NTE.TabIndex = 23;
+            this.NTE.Text = "27";
+            // 
+            // NN
+            // 
+            this.NN.AutoSize = true;
+            this.NN.Location = new System.Drawing.Point(349, 91);
+            this.NN.Name = "NN";
+            this.NN.Size = new System.Drawing.Size(13, 13);
+            this.NN.TabIndex = 24;
+            this.NN.Text = "0";
+            // 
+            // NNE
+            // 
+            this.NNE.AutoSize = true;
+            this.NNE.Location = new System.Drawing.Point(349, 118);
+            this.NNE.Name = "NNE";
+            this.NNE.Size = new System.Drawing.Size(13, 13);
+            this.NNE.TabIndex = 25;
+            this.NNE.Text = "0";
+            // 
+            // NCO
+            // 
+            this.NCO.AutoSize = true;
+            this.NCO.Location = new System.Drawing.Point(349, 145);
+            this.NCO.Name = "NCO";
+            this.NCO.Size = new System.Drawing.Size(13, 13);
+            this.NCO.TabIndex = 26;
+            this.NCO.Text = "0";
+            // 
+            // NS
+            // 
+            this.NS.AutoSize = true;
+            this.NS.Location = new System.Drawing.Point(349, 198);
+            this.NS.Name = "NS";
+            this.NS.Size = new System.Drawing.Size(13, 13);
+            this.NS.TabIndex = 27;
+            this.NS.Text = "0";
+            // 
+            // NSE
+            // 
+            this.NSE.AutoSize = true;
+            this.NSE.Location = new System.Drawing.Point(349, 171);
+            this.NSE.Name = "NSE";
+            this.NSE.Size = new System.Drawing.Size(13, 13);
+            this.NSE.TabIndex = 28;
+            this.NSE.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NSE);
+            this.Controls.Add(this.NS);
+            this.Controls.Add(this.NCO);
+            this.Controls.Add(this.NNE);
+            this.Controls.Add(this.NN);
+            this.Controls.Add(this.NTE);
+            this.Controls.Add(this.BS);
+            this.Controls.Add(this.BSE);
+            this.Controls.Add(this.BCO);
+            this.Controls.Add(this.BNE);
+            this.Controls.Add(this.BN);
+            this.Controls.Add(this.BTE);
+            this.Controls.Add(this.TE);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.NE);
+            this.Controls.Add(this.CO);
+            this.Controls.Add(this.SEc);
+            this.Controls.Add(this.Sc);
+            this.Controls.Add(this.S);
+            this.Controls.Add(this.Se);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Se;
+        private System.Windows.Forms.Label S;
+        private System.Windows.Forms.ComboBox Sc;
+        private System.Windows.Forms.ComboBox SEc;
+        private System.Windows.Forms.ComboBox CO;
+        private System.Windows.Forms.ComboBox NE;
+        private System.Windows.Forms.ComboBox N;
+        private System.Windows.Forms.Button BTE;
+        private System.Windows.Forms.Button BN;
+        private System.Windows.Forms.Button BNE;
+        private System.Windows.Forms.Button BCO;
+        private System.Windows.Forms.Button BSE;
+        private System.Windows.Forms.Button BS;
+        private System.Windows.Forms.Label NTE;
+        private System.Windows.Forms.Label NN;
+        private System.Windows.Forms.Label NNE;
+        private System.Windows.Forms.Label NCO;
+        private System.Windows.Forms.Label NS;
+        private System.Windows.Forms.Label NSE;
+        private System.Windows.Forms.ComboBox TE;
+    }
+}
+
---
+++ Estados/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estados
+{
+    public partial class Form1 : Form
+    {
+        int ne1 = 0;
+        int ne2 = 0;
+        int ne3 = 0;
+        int ne4 = 0;
+        int ne5 = 0;
+        int ne = 27;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTE_Click(object sender, EventArgs e)
+        {
+            string Estado = TE.Text;
+            
+
+            if(Estado == "Rio Grande do Sul - RS" || Estado == "Santa Catarina - SC" || Estado == "Paraná - PR")
+            {
+                Sc.Items.Add(Estado);
+                TE.Items.Remove(TE.Text);
+                ne1++;
+                NS.Text = ne1.ToString();
+            }
+            else if (Estado == "Minas Gerais - MG" || Estado == "Espírito Santo - ES" || Estado == "São Paulo - SP" || Estado == "Rio de Janeiro - RJ")
+            {
+                SEc.Items.Add(Estado);
+                TE.Items.Remove(TE.Text);
+                ne2++;
+                NSE.Text = ne2.ToString();
+            }
+            else if (Estado == "Mato Grosso - MT" || Estado == "Mato Grosso do Sul - MS" || Estado == "Goiás - GO" || Estado == "Distrito Federal - DF")
+            {
+                CO.Items.Add(Estado);
+                TE.Items.Remove(TE.Text);
+                ne3++;
+                NCO.Text = ne3.ToString();
+            }
+            else if (Estado == "Sergipe - SE" || Estado == "Rio Grande do Norte - RN"|| Estado == "Piauí - PI" || Estado == "Maranhão - MA" || Estado == "Bahia - BA" || Estado == "Paraíba - PB" || Estado == "Pernambuco - PE" || Estado == "Ceará - CE" || Estado == "Tocantins - TO" || Estado == "Alagoas - AL")
+            {
+                NE.Items.Add(Estado);
+                TE.Items.Remove(TE.Text);
+                ne4++;
+                NNE.Text = ne4.ToString();
+            }
+            else if (Estado == "Acre - AC" || Estado == "Amazonas - AM" || Estado == "Pará - PA" || Estado == "Roraima - RR" || Estado == "Rondônia - RO" || Estado == "Amapá - AP")
+            {
+                N.Items.Add(Estado);
+                TE.Items.Remove(TE.Text);
+                ne5++;
+                NN.Text = ne5.ToString();
+            }
+
+            ne--;
+            NTE.Text = ne.ToString();
+        }
+    }
+}
